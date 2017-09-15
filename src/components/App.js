cec9d2910670598d5e07b065e336100311b475cd
iamninja <bagios234@gmail.com>
2017-09-15 20:05:18 +0300

    Part 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import '../App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
+        <Header text="iamninja's Movie Mojo App!" />
         <p className="App-intro">
           Welcome to the 'Movie Mojo oMDb' React app!
         </p>
