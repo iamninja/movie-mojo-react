9676e06488378818f40b73de2a61131173ca5e6a
iamninja <bagios234@gmail.com>
2017-09-15 21:25:08 +0300

    Part 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Movie from './Movie';
+import {initialMovies} from '../movies';
+import {additionalMovies} from '../movies';
 import '../App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: initialMovies
+    };
+
+    this.loadAdditionalMovies = this.loadAdditionalMovies.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
         <Header text="iamninja's Movie Mojo App!" />
-        <p className="App-intro">
-          Welcome to the 'Movie Mojo oMDb' React app!
-        </p>
+        <p className="App-intro">Sharing a few of our favourite movies</p>
+        <div className="movies">
+          {
+            Object
+              .keys(this.state.movies)
+              .map(key => <Movie key={key} meta={ this.state.movies[key] } />)
+          }
+        </div>
+
+        <div className="add-movies">
+          <button onClick={ this.loadAdditionalMovies }>Load more...</button>
+        </div>
       </div>
     );
+  }
+
+  loadAdditionalMovies() {
+    var currentMovies = { ...this.state.movies }
+    var newMovies = Object.assign( currentMovies, additionalMovies );
+
+    this.setState({ movies: newMovies });
   }
 }
 
